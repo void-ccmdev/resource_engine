384f1ea8b44bdc16bb51c83c0a4f2f479517dd84
𝖈𝖈𝖒𝖉𝖊𝖛_ <controlcommanddev@gmail.com>
2026-09-03 12:28:46 +0200

    Fixed Input class!
    Input class is now in a usable state!

--- src/input/input.cpp
+++ src/input/input.cpp
@@ -1,4 +1,5 @@
 #include "input.h"
+#include "GLFW/glfw3.h"
 
 #include <stdexcept>
 
@@ -13,7 +14,30 @@ void Input::addEvent(InputEvent newEvent)
     eventsCount++;
 }
 
-void Input::processInput()
+void Input::processInput(GLFWwindow* window)
 {
-    
+    for (unsigned int i = 0; eventsCount; i++) {
+        if (events[i].device == KEYBOARD) {
+            if (glfwGetKey(window, events[i].key) != events[i].state) continue;
+        }
+        else if (events[i].device == MOUSE) {
+            if (glfwGetMouseButton(window, events[i].key) != events[i].state) continue;
+        }
+
+        const int ALT = glfwGetKey(window, GLFW_KEY_LEFT_ALT) == GLFW_PRESS or
+            glfwGetKey(window, GLFW_KEY_RIGHT_ALT) == GLFW_PRESS;
+        const int CTRL = glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS or
+            glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS;
+        const int SHIFT = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS or
+            glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
+        const int SUPER = glfwGetKey(window, GLFW_KEY_LEFT_SUPER) == GLFW_PRESS or
+            glfwGetKey(window, GLFW_KEY_RIGHT_SUPER) == GLFW_PRESS;
+
+        if (events[i].mod.alt and events[i].mod.alt != ALT) continue;
+        if (events[i].mod.ctrl and events[i].mod.ctrl != CTRL) continue;
+        if (events[i].mod.shift and events[i].mod.shift != SHIFT) continue;
+        if (events[i].mod.super and events[i].mod.super != SUPER) continue;
+
+        events[i].action();
+    }
 }
--- src/input/input.h
+++ src/input/input.h
@@ -1,28 +1,36 @@
 #pragma once
 
 #include <GLFW/glfw3.h>
+#include <functional>
 
 namespace Input {
-    const struct Mod
+    struct Mod
     {
         bool alt = false;
         bool ctrl = false;
         bool shift = false;
         bool super = false;
     };
-    
-    const struct InputEvent
+
+    enum DEVICE {
+        KEYBOARD,
+        MOUSE
+    };
+
+    struct InputEvent
     {
+        DEVICE device = KEYBOARD;
         int key;
         Mod mod;
         int state = GLFW_PRESS;
-        const void* action;
+        std::function<void()> action;
     };
-    
+
+
     InputEvent events[64];
     unsigned int eventsCount = 0;
 
     void addEvent(InputEvent newEvent);
-    void processInput();
+    void processInput(GLFWwindow* window);
 
 } //namespace Input
