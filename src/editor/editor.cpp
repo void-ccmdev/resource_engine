04e7d3ec85fdb8d2d149808d6af3d659b29fb7a1
ccmdev <controlcommanddev@gmail.com>
2026-09-08 11:36:06 +0200

    Attempted to set up IMGUI

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -1,6 +1,9 @@
 #include "editor.h"
 
 #include <iostream>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 using namespace Editor;
 
@@ -16,13 +19,35 @@ int EditorApp::run()
     
     m_window.create(1280, 720, title);
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsDark();
+    bool show_demo_window = true;
     
     m_window.setClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     while (running && !m_window.shouldClose()) {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (show_demo_window) {
+            ImGui::ShowDemoWindow(&show_demo_window);
+        }
+
+
+        ImGui::Render();
         m_inputManager.processInput(m_window.getGlfwWindow());
         m_window.update();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
     
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     m_window.close();
 
     running = false;
--- src/engine/core/window.cpp
+++ src/engine/core/window.cpp
@@ -12,6 +12,10 @@ void Window::create(unsigned int width, unsigned int height, std::string& title)
         std::runtime_error("Failed to initialize GLFW!");
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     m_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
     if (m_window == nullptr) {
         glfwTerminate();
